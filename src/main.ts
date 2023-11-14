1319f374f7307ebff15d5a536fb9d43d11aa2518
Dietrich Stein <dietrich.stein@gmail.com>
2023-11-13 20:16:32 -0500

    Fixes the demo script with new eslint powers

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,17 @@ console.log('hello');
 window.addEventListener('load', init);
 
 function init() {
-  let el = document.getElementById('root');
+  const el = document.getElementById('root');
   if (el === null) {
     return;
   }
 
-  let message = `one\n` + `   two\n` + `      three\n`;
+  const message = 'one\n   two\n      three\n';
 
   el.innerHTML = `<span id="output" contenteditable="true" spellcheck="false">${message}</span>`;
 
   setTimeout(() => {
-    let elOutput = document.getElementById('output');
+    const elOutput = document.getElementById('output');
     if (elOutput === null) {
       return;
     }
